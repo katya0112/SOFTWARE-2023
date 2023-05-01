5ce3c9154e0fb1545588cad62c7605530eaab6db
katya0112 <hdzkatya07@gmail.com>
2023-05-01 17:09:25 -0600

    Segundo commit

---
+++ UberPOO/JS/account.js
@@ -0,0 +1,15 @@
+Account {name: "Andrea Sanchez", document: "ASD123365214", email:"andrea@platzi.pe", password:"2365214"}
+    Andrea Sanchez
+    ASD123365214
+    UberX
+    
+Account {name: "Marcos Caceres", document: "DFG236589", email: "marcosc@platzi.pe", password:"569324"}
+    Marcos Caceres
+    DFG236589
+    toyota
+    corolla
+    ****User****
+    Nombre: Damian Rios
+    Document: KLOIS12536
+    E-mail: damian@platzi.pe
+    Password: 523698563
---
+++ UberPOO/Java/User.java
@@ -0,0 +1,15 @@
+class User extends Account { 
+    public User(String name, String 
+    document, String email, String password){
+        super(name, document, email, password);
+    
+    }
+    void printDataUser() {
+     System.out.println("Document
+     driver: + name + " E-mail:
+     driver: " + document + Name + email + Password: "+ "
+     password);
+    
+    }
+    
+    }

a2c4d5e77c7d8bb7877866bf43f5dc1df92b4d0d
katya0112 <hdzkatya07@gmail.com>
2023-05-01 03:22:52 -0600

    primer commit

---
+++ UberPOO/Java/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Inicializando...");
+        System.out.println("Car...");
+        Car car = new Car("AMQ123", new Account("Andres Loiza",
+        "ADN1235", "andresl@hotmail.com", "12365")
+    }
+}

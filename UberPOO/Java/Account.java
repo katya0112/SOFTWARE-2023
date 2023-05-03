1a40a298719fbe458c2a78e44197a9abed5cf61d
katya0112 <hdzkatya07@gmail.com>
2023-05-03 05:48:53 -0600

    five commit

--- UberPOO/Java/Account.java
+++ UberPOO/Java/Account.java
@@ -1,3 +1,9 @@
 public class Account {
-    
+    public Account(String string, String string2, String string3, String string4) {
+    }
+    Integer id;
+    String name;
+    String document;
+    String email;
+    String password;
 }
